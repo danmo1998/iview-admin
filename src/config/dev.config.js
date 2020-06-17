f79d541bcaa9de649c4da39a7b748482d6a90555
danmo1998 <1154065417@qq.com>
2020-06-17 16:09:27 +0800

    测试

---
+++ src/config/dev.config.js
@@ -0,0 +1,6 @@
+export default {
+    RTYL_PHP:'http://localhost:8088',
+    RTYX_JAVA : 'http://localhost:8077',
+    RTYXM_JAVA : 'http://localhost:8066',
+    RTYCMS_JAVA : 'http://localhost:8055'
+}
---
+++ src/config/product.config.js
@@ -0,0 +1,6 @@
+export default {
+    RTYL_PHP:'http://fory.top:8088',
+    RTYX_JAVA : 'http://fory.top:8077',
+    RTYXM_JAVA : 'http://fory.top:8066',
+    RTYCMS_JAVA : 'http://fory.top:8055'
+}
